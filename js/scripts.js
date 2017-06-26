5446f163c861c4e379c67eb79459031a2fe3c3bf
Patrick Napper <patrick@patricknapper.com>
2017-06-26 15:41:18 -0700

    add js front end and back end logic for specifications.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,30 @@
+var countUp = [""];
+var pingPong = function(number) {
+  for (var i = 1; i <= number; i ++) {
+    alert(i);
+    countUp += number[i];
+    console.log(countUp);
+    if (i % 3 === 0) {
+      return "ping";
+    } else if (i % 5 === 0) {
+      return "pong";
+    } else if (i % 15 === 0) {
+      return "pingpong";
+    } else {
+      return "Please enter a number.";
+    }
+  }
+    console.log(pingPong);
+    alert(a);
+};
 
+$(document).ready(function() {
+  $("form#ping-pong").submit(function(event) {
+    event.preventDefault();
+    var number = parseInt($("input#number").val());
+    var output = pingPong(number);
+    alert(b);
+    $("#countUp").append(countUp);
+      $("#output").text(output);
+  });
+});
