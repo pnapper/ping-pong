1b9f9bdf04ff8e6ade3e5af19fb37c25c922c120
Patrick Napper <patrick@patricknapper.com>
2017-06-28 10:10:27 -0700

    add changes to scipts.js to display output from user input.

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,26 @@
-var countUp = [""];
+var calcResult = function(i) {
+  if (i % 15 === 0) {
+    return "pingpong";
+  } else if (i % 5 === 0) {
+    return "pong";
+  } else if (i % 3 === 0) {
+    return "ping";
+  } else {
+    return i;
+  }
+}
+
 var pingPong = function(number) {
   for (var i = 1; i <= number; i ++) {
-    alert(countUp);
-    countUp += number[i];
-    console.log(countUp);
-    if (i % 3 === 0) {
-      return "ping";
-    } else if (i % 5 === 0) {
-      return "pong";
-    } else if (i % 15 === 0) {
-      return "pingpong";
-    } else {
-      return "Please enter a number.";
-    }
+    //console.log(calcResult(i));
+    $("#countUp").append(calcResult(i) + "<br>");
   }
-    console.log(pingPong);
-    alert(2);
-};
+}
 
 $(document).ready(function() {
   $("form#ping-pong").submit(function(event) {
     event.preventDefault();
     var number = parseInt($("input#number").val());
-    var output = pingPong(number);
-    alert(3);
-    $("#countUp").append(countUp);
-      $("#output").text(output);
+    pingPong(number);
   });
 });
